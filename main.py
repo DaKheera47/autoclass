2931503c171fbc7e32a925fcd84119ff4e7aad37
DaKheera47 <shaheergoodboy@gmail.com>
2021-07-06 08:09:42 +0500

    Logging

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import yaml
 from launcher import main as launcherMain
 from launcher import clear
 import schedule
-import datetime
+from datetime import datetime
 from termcolor import cprint
 import cursor
+import json
 cursor.hide()
 
 # importing external files
@@ -33,7 +34,7 @@ def getCodeAndPass(cls):
 def main():
     clear()
 
-    currTime = datetime.datetime.now().strftime("%H:%M")
+    currTime = datetime.now().strftime("%H:%M")
 
     cprint(f"Time: {currTime}", "cyan", attrs=["bold"], end="\r")
 
@@ -43,7 +44,8 @@ def main():
 
     for cls in CLASS_INFO.items():
         if currTime == cls[1]["time_weekday"]:
-            # if "10:45" == cls[1]["time_weekday"]:
+        # if "Chemistry" == cls[0]:
+            # Uncomment to add confirmation
             # isConfirmed = pag.confirm(
             #     text=f'Join {cls[0]} class?', title=f'Confirm joining of {cls[0]} class', buttons=['OK', 'Cancel'])
 
@@ -51,10 +53,28 @@ def main():
             code_to_use, password_to_use = getCodeAndPass(cls[0])
             print(
                 f"\nUsing {cls[0]} class information \n  Code: {code_to_use} \n  Pass: {password_to_use}")
+
+            # sending the program into execution phase
             launcherMain(code_to_use, password_to_use)
 
+            # logging classes entered
+            currClassInfo = {
+                "time": currTime,
+                "className": cls[0],
+                "date": datetime.today().strftime('%Y-%m-%d')
+            }
+            with open("log.json", "r+") as f:
+                output = []
+                fileData = json.loads(f.read())
+                for entry in fileData:
+                    output.append(entry)
+                output.append(currClassInfo)
 
-schedule.every(30).seconds.do(main)
+                f.seek(0)
+                json.dump(output, f)
+
+
+schedule.every(5).seconds.do(main)
 
 while True:
     schedule.run_pending()
