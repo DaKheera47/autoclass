0ac18a7654f7e815e70274a73c8b5efc66a1b30e
DaKheera47 <shaheergoodboy@gmail.com>
2021-09-10 22:22:02 +0500

    Perfecting first install script

--- build/firstInstall.bat
+++ build/firstInstall.bat
@@ -1,5 +1,18 @@
+@REM elevating to admin
+@REM https://stackoverflow.com/questions/6811372/how-to-code-a-bat-file-to-always-run-as-admin-mode/45382540
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+
+@REM installing choco
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+@REM installing programs required
 choco install python -y
 choco install obs-studio -y
+choco install zoom -y
+
+@REM refreshing all environment variables to make sure python works
 refreshenv
+
+@REM installing python packages required
 pip install -r ../requirements.txt
