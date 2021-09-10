8e486d66c7bccb6349af1aff733466f414836a16
DaKheera47 <shaheergoodboy@gmail.com>
2021-09-10 20:46:03 +0500

    firstInstallation.Bat File Generated

--- build/firstInstall.bat
+++ build/firstInstall.bat
@@ -1,4 +1,5 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-choco install python
-choco install obs-studio
-pip install -r requirements.txt
+choco install python -y
+choco install obs-studio -y
+refreshenv
+pip install -r ../requirements.txt
