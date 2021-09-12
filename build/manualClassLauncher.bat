f7ea4daa16be86db86e14d75018a1587893236be
DaKheera47 <shaheergoodboy@gmail.com>
2021-09-12 21:40:31 +0500

    Stopping program after error for easier debugging

--- build/manualClassLauncher.bat
+++ build/manualClassLauncher.bat
@@ -1,2 +1,4 @@
 pip install -r ../requirements.txt
 python ../launcher.py
+echo An error occured. Please save a screenshot of the text above as a crash report.
+pause
--- build/scheduledClassLauncher.bat
+++ build/scheduledClassLauncher.bat
@@ -1,2 +1,4 @@
 pip install -r ../requirements.txt
 python ../main.py
+echo An error occured. Please save a screenshot of the text above as a crash report.
+pause
