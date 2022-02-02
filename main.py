1a3ad27259b9cfe1ac9618b01349474ad35f98fd
Shaheer Sarfaraz <shaheergoodboy@gmail.com>
2022-02-02 15:01:16 +0500

    Main updated to work with custom table version of launcher

--- main.py
+++ main.py
@@ -10,10 +10,12 @@ from functools import partial
 import cursor
 cursor.hide()
 
-pag.LOG_SCREENSHOTS = True  # If True, save screenshots for clicks and key presses.
+# If True, save screenshots for clicks and key presses.
+pag.LOG_SCREENSHOTS = True
 
 # If not None, PyAutoGUI deletes old screenshots when this limit has been reached:
 pag.LOG_SCREENSHOTS_LIMIT = 50
+
 
 def checkForClassTime():
     SETUP, CLASS_INFO, _ = loadFiles()
@@ -24,20 +26,17 @@ def checkForClassTime():
     genTable(CLASS_INFO)
 
     for cls in CLASS_INFO.items():
-        print(cls[0])
         # getting class code
         code_to_use = str(CLASS_INFO[cls[0]]["code"])
         password_to_use = str(CLASS_INFO[cls[0]]["password"])
 
         # checking if today is between monday and thursday before joining
         if CURR_TIME == cls[1]["time_weekday"] and CURR_DAY_NUM in range(0, 4):
-            EVENT_LOOP.append(
-                (partial(startLaunching, cls[0], code_to_use, password_to_use)))
+            EVENT_LOOP.append((partial(startLaunching, cls[0])))
 
         # checking if today is friday before joining
         if CURR_TIME == cls[1]["time_friday"] and CURR_DAY_NUM == 4:
-            EVENT_LOOP.append(
-                (partial(startLaunching, cls[0], code_to_use, password_to_use)))
+            EVENT_LOOP.append((partial(startLaunching, cls[0])))
 
         # checking if today is friday before leaving
         if CURR_TIME == cls[1]["time_of_leaving_friday"] and CURR_DAY_NUM == 4:
